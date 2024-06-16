1e2f4bfe72f64eb5d02f5b37f9a179fc6c71a5bb
jasonlin1993 <ninjachen.lin@gmail.com>
2024-06-16 16:55:50 +0800

    Tutorial4

--- hyrule-job/src/App.vue
+++ hyrule-job/src/App.vue
@@ -1,31 +1,51 @@
 <template>
-  <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('zelda')">change name</button>
-    <button @click="changeAge('35')">change name</button>
-  </div>
+  <div class="app"></div>
+  <JobList :jobs="jobs" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import Job from "./types/Job";
+import JobList from "./components/JobList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
-  data() {
-    return {
-      name: "Link",
-      age: 25 as number | string,
-    };
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-      return name;
-    },
-    changeAge(age: number | string) {
-      this.age = age;
-    },
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      {
+        title: "fisherman",
+        location: "lake hylia",
+        salary: 21000,
+        id: "4",
+      },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    return { jobs };
   },
 });
 </script>
