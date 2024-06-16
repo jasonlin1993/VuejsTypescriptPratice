be439c6093652660503c51ed3510880c4c2822f1
jasonlin1993 <ninjachen.lin@gmail.com>
2024-06-16 12:48:26 +0800

    Tutorial2

---
+++ hyrule-job/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('zelda')">change name</button>
+    <button @click="changeAge('35')">change name</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: number | string) {
+      this.age = age;
+    },
+  },
+});
+</script>
+
+<style></style>
